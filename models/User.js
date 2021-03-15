d11e0b12c610a005928db2a00b0ca3d21a211b47
Rachana R Shankar <69310927+Raptor363@users.noreply.github.com>
2021-03-15 11:46:29 +0530

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,18 +1,3 @@
-// const mongoose = require('mongoose');
-// const Schema = mongoose.Schema;
-// const passportLocalMongoose = require('passport-local-mongoose');
-
-// const UserSchema = new Schema({
-//     email: {
-//         type: String,
-//         required: true,
-//         unique: true
-//     }
-// });
-
-// UserSchema.plugin(passportLocalMongoose);
-
-// module.exports = mongoose.model('User', UserSchema);
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
